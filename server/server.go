2e7674d81dedb03040767faeebcb2e82916425a2
Crismar Mejia <crismar.mejia@gmail.com>
2016-09-25 23:04:17 -0400

    implemented basic observer. Organized the server code better

--- server/server.go
+++ server/server.go
@@ -4,9 +4,51 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 )
 
-func Run() {
+type Server struct {
+	list map[string]net.Conn
+	mux  sync.Mutex
+}
+
+func New() Server {
+	return Server{list: make(map[string]net.Conn)}
+}
+
+func (s *Server) register(conn net.Conn) {
+	s.mux.Lock()
+	s.list[conn.RemoteAddr().String()] = conn
+	s.mux.Unlock()
+}
+
+func (s *Server) unregister(conn net.Conn) {
+	s.mux.Lock()
+	delete(s.list, conn.RemoteAddr().String())
+	s.mux.Unlock()
+}
+
+func (s *Server) messageReader(conn net.Conn) {
+	//read buff
+	buffer := make([]byte, 1024)
+	blen, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message := string(buffer[:blen])
+	// This quits the connetion at least when using netcat
+	//TODO look fo the right way to close
+	if message == "/quit" {
+		fmt.Println("quit command recieved. Bye.")
+		//unregister
+		s.unregister(conn)
+		return
+	}
+	fmt.Println(message)
+}
+
+func (s *Server) Run() {
 	// Listen on port TPC 2016
 	listener, err := net.Listen("tcp", ":2016")
 	if err != nil {
@@ -20,26 +62,11 @@ func Run() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		//register the client
+		s.register(conn)
+		fmt.Println("registered local " + conn.LocalAddr().String() + "   |----|  remote " + conn.RemoteAddr().String())
 
 		//run goroutines to deal with multiple connections
-		go messageReader(conn)
+		go s.messageReader(conn)
 	}
-}
-
-func messageReader(conn net.Conn) {
-	//read buff
-	buffer := make([]byte, 1024)
-	blen, err := conn.Read(buffer)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	message := string(buffer[:blen])
-	// This quits the connetion at least when using netcat
-	//TODO look fo the right way to close
-	if message == "/quit" {
-		fmt.Println("quit message recieved. Bye.")
-		return
-	}
-	fmt.Println(message)
 }
